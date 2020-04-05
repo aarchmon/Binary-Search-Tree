900c6cb82eb4e5a6403267825a1082407d878cfc
Aaron Christopher Montano <amonta1994@aarons-mbp.home>
2020-04-04 21:18:17 -0700

    Added data entry into main().

--- source/Runner.cpp
+++ source/Runner.cpp
@@ -13,6 +13,25 @@
  */
 int main(void) {
 
+	// Binary Search Tree root.
+	TreeNode tree, *root = NULL;
+
+	// Welcome message. Prompt user to insert an unsigned integer value to be used as a root and
+	// inform user to enter -1 to end data entry.
+	unsigned int input;
+	std::cout << "Welcome to Binary Search Tree. Please enter an unsigned integer value to be ";
+	std::cout << "the root value: ";
+	std::cin >> input;
+	root = tree.insert(root, input);
+
+	// Prompt user to enter additional data into the Binary Search Tree.
+	// Enter any negative integer to end data entry.
+	while(input != -1) {
+		std::cout << "Enter additional data (enter any negative integer to stop data entry): ";
+		std::cin >> input;
+		tree.insert(root, input);
+	}
+
 	return 0;
 }
 
